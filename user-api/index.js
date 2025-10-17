fd8a749a7e152877561f21f675e6b1ea99b4c0a9
José Carlos Pesina Siller <josecarlospesinasiller@Joses-MacBook-Air.local>
2025-10-17 15:57:46 +0400

    endpoint for single user

--- user-api/index.js
+++ user-api/index.js
@@ -1,47 +1,169 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
 
 const users = [
-  { name: "carlos", profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg", country: "mexico", age: 38, gender: "male" },
-  { name: "mahdia", profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg", country: "Afghanistan", age: 22, gender: "female" },
-  { name: "carlos", profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg", country: "mexico", age: 38, gender: "male" },
-  { name: "mahdia", profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg", country: "Afghanistan", age: 22, gender: "female" },
-    { name: "carlos", profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg", country: "mexico", age: 38, gender: "male" },
-  { name: "mahdia", profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg", country: "Afghanistan", age: 22, gender: "female" },
-    { name: "carlos", profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg", country: "mexico", age: 38, gender: "male" },
-  { name: "mahdia", profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg", country: "Afghanistan", age: 22, gender: "female" },
-    { name: "carlos", profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg", country: "mexico", age: 38, gender: "male" },
-  { name: "mahdia", profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg", country: "Afghanistan", age: 22, gender: "female" },
-    { name: "carlos", profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg", country: "mexico", age: 38, gender: "male" },
-  { name: "mahdia", profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg", country: "Afghanistan", age: 22, gender: "female" },
-    { name: "carlos", profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg", country: "mexico", age: 38, gender: "male" },
-  { name: "mahdia", profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg", country: "Afghanistan", age: 22, gender: "female" },
-    { name: "carlos", profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg", country: "mexico", age: 38, gender: "male" },
-  { name: "mahdia", profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg", country: "Afghanistan", age: 22, gender: "female" },
+  {
+    id: 0,
+    name: "carlos",
+    profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg",
+    country: "mexico",
+    age: 38,
+    gender: "male",
+  },
+  {
+    id: 1,
+    name: "mahdia",
+    profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg",
+    country: "Afghanistan",
+    age: 22,
+    gender: "female",
+  },
+  {
+    id: 3,
+    name: "carlos",
+    profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg",
+    country: "mexico",
+    age: 38,
+    gender: "male",
+  },
+  {
+    id: 4,
+    name: "mahdia",
+    profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg",
+    country: "Afghanistan",
+    age: 22,
+    gender: "female",
+  },
+  {
+    id: 5,
+    name: "carlos",
+    profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg",
+    country: "mexico",
+    age: 38,
+    gender: "male",
+  },
+  {
+    id: 6,
+    name: "mahdia",
+    profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg",
+    country: "Afghanistan",
+    age: 22,
+    gender: "female",
+  },
+  {
+    id: 7,
+    name: "carlos",
+    profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg",
+    country: "mexico",
+    age: 38,
+    gender: "male",
+  },
+  {
+    id: 8,
+    name: "mahdia",
+    profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg",
+    country: "Afghanistan",
+    age: 22,
+    gender: "female",
+  },
+  {
+    id: 9,
+    name: "carlos",
+    profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg",
+    country: "mexico",
+    age: 38,
+    gender: "male",
+  },
+  {
+    id: 10,
+    name: "mahdia",
+    profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg",
+    country: "Afghanistan",
+    age: 22,
+    gender: "female",
+  },
+  {
+    id: 11,
+    name: "carlos",
+    profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg",
+    country: "mexico",
+    age: 38,
+    gender: "male",
+  },
+  {
+    id: 12,
+    name: "mahdia",
+    profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg",
+    country: "Afghanistan",
+    age: 22,
+    gender: "female",
+  },
+  {
+    id: 13,
+    name: "carlos",
+    profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg",
+    country: "mexico",
+    age: 38,
+    gender: "male",
+  },
+  {
+    id: 14,
+    name: "mahdia",
+    profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg",
+    country: "Afghanistan",
+    age: 22,
+    gender: "female",
+  },
+  {
+    id: 15,
+    name: "carlos",
+    profilePicture: "https://cdn2.thecatapi.com/images/DBmIBhhyv.jpg",
+    country: "mexico",
+    age: 38,
+    gender: "male",
+  },
+  {
+    id: 16,
+    name: "mahdia",
+    profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg",
+    country: "Afghanistan",
+    age: 22,
+    gender: "female",
+  },
+    {
+    id: 17,
+    name: "mahdia",
+    profilePicture: "https://cdn2.thecatapi.com/images/FCNqMC83P.jpg",
+    country: "Afghanistan",
+    age: 22,
+    gender: "female",
+  },
 ];
 
+app.get("/user/", (req, res) => {
+  console.log(req.query);
+  const id = req.query.userId;
 
-app.get('/user/', (req, res) => {
-    console.log(req.query)
-    const name = req.query.name; 
-    console.log("input from user is: " + name);
-    const user = users.find(u => u.name === name) 
-    res.json(user);
+  console.log("input from user is: " + id);
+
+  const user = users.find((user) => user.id ===  parseInt(id));
+  console.log("user found: ", user);
+  res.json(user);
 });
 
-app.get('/users/', (req, res) => {
-    res.json(users);
+
+
+app.get("/users/", (req, res) => {
+  res.json(users);
 });
 
-
-app.get('/error/', (req, res) => {
-    res.json("error");
+app.get("/error/", (req, res) => {
+  res.json("error");
 });
-
 
 app.listen(3000, () => {
-  console.log('Listening on http://127.0.0.1:3000');
+  console.log("Listening on http://127.0.0.1:3000");
 });
